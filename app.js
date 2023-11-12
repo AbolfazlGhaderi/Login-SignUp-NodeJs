0eff29bc88a6253d991683572679aa024c3be139
mr.Brian <brianprogrammer0@gmail.com>
2023-11-12 16:52:11 +0330

    add-> tamplate engin EJS

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
 
-app.get("/",( req, res) => {
+//----------- Set Views ------------------
 
-    res.send("Hello World !")
+app.set("view engin", "ejs");
+app.set("views", "views");
+
+//-----------------------------
+
+//------------------- Middlewares --------------------------------
+
+
+
+//-----------------
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World !");
 });
 
-
-app.listen(3000,console.log("App is Runing in port 3000 !"))
+app.listen(3000, console.log("App is Runing in port 3000 !"));
