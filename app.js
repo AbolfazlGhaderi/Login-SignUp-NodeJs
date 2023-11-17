fdbd5d8627ea7ae85b862dbe7dd24a87396b09f1
mr.Brian <brianprogrammer0@gmail.com>
2023-11-17 16:52:58 +0330

    Add -> Flash And Sweet Alert

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 
 const bodyParser=require('body-parser');
 const path =require('path');
+const flash = require('connect-flash');
+const cookiParser = require('cookie-parser');
+const session = require('express-session');
 
 const authRoute=require('./routes/auth');
 //----------- Set Views ------------------
@@ -14,12 +17,25 @@ app.set("views", "views");
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use("/assets",express.static(path.join(__dirname,'public')))
+//----------- flash ----------------------
+app.use(cookiParser());
+app.use(session({
+    secret : 'something',
+    cookie: { maxAge: 60000 },
+    resave: true,
+    saveUninitialized: true
+  }));
+app.use(flash());
 
 //----------- Router ---------------------
 
 app.use(authRoute)
 
 app.get('/',(req,res)=>{
-res.render('./index.ejs')
+   
+    const successAddProduct = req.flash("successAddProduct");
+
+    res.render('./index.ejs',{successAddProduct})
 })
+
 app.listen(3000, console.log("App is Runing in port 3000 !"));
--- controllers/auth.js
+++ controllers/auth.js
@@ -3,11 +3,11 @@ const jdate =require('jalali-date');
 
 
 exports.getSingin = (req,res)=>{
-
+    
     res.render('../views/Singin.ejs')
 }
 
 exports.getSingup = (req,res)=>{
-
-    res.render('../views/Singup.ejs')
+    let successAddProduct=req.flash("successAddProduct","test");
+    res.redirect('/')
 }
