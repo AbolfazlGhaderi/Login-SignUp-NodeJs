ee579e3de18f49366f1c1a19610c69deeb702ce3
mr.Brian <brianprogrammer0@gmail.com>
2023-11-20 09:23:24 +0330

    Add -> Connect to MongoDB

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = express();
 
-const bodyParser=require('body-parser');
-const path =require('path');
+const bodyParser = require('body-parser');
+const path = require('path');
 const flash = require('connect-flash');
 const cookiParser = require('cookie-parser');
 const session = require('express-session');
 
-const authRoute=require('./routes/auth');
+const authRoute = require('./routes/auth');
+const { mongo, Mongoose } = require("mongoose");
+
+const mongoose = require("mongoose")
 //----------- Set Views ------------------
 
 app.set("view engine", "ejs");
@@ -15,27 +18,32 @@ app.set("views", "views");
 
 //----------- Middlewares ----------------
 
-app.use(bodyParser.urlencoded({extended:false}))
-app.use("/assets",express.static(path.join(__dirname,'public')))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use("/assets", express.static(path.join(__dirname, 'public')))
 //----------- flash ----------------------
 app.use(cookiParser());
 app.use(session({
-    secret : 'something',
-    cookie: { maxAge: 60000 },
-    resave: true,
-    saveUninitialized: true
-  }));
+  secret: 'something',
+  cookie: { maxAge: 60000 },
+  resave: true,
+  saveUninitialized: true
+}));
 app.use(flash());
 
 //----------- Router ---------------------
 
 app.use(authRoute)
 
-app.get('/',(req,res)=>{
-   
-    const successAddProduct = req.flash("successAddProduct");
+app.get('/', (req, res) => {
 
-    res.render('./index.ejs',{successAddProduct})
+  const successAddProduct = req.flash("successAddProduct");
+
+  res.render('./index.ejs', { successAddProduct })
 })
 
-app.listen(3000, console.log("App is Runing in port 3000 !"));
+mongoose.connect('mongodb://127.0.0.1:27017/login-signup-nodejs')
+  .then(() => {
+    console.log("connected to login-signup-nodejs DB ... ");
+    app.listen(3000, console.log("App is Runing in port 3000 !"));
+  })
+
