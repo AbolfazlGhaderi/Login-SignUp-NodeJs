73e6130a4cb44ac9a3cd218e05fe9ed62fc84b17
mr.Brian <brianprogrammer0@gmail.com>
2023-11-23 15:05:44 +0330

    reset

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,6 @@
 const bodyParser = require('body-parser');
 const jdate =require('jalali-date');
-<<<<<<< HEAD
 
-=======
-const {check,validationResult} = require('express-validator');
->>>>>>> 5f2a199f6b66f6b8b7fe4048eee24e7e6ea7923f
 // ---------------------- GET ----------------------
 exports.getSingin = (req,res)=>{
     
@@ -21,21 +17,5 @@ exports.getSingup = (req,res)=>{
 // ---------------------- Post ----------------------
 
 exports.postSingup =(req,res)=>{
-<<<<<<< HEAD
 
-=======
-    const body = req.body;
-
-    console.log(body);
-
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        console.log(errors.array()[0].msg);
-
-        return res.render('../views/Singup.ejs',{
-            validateError:errors.array()[0].msg
-        })
-    }
-    res.json(body)
->>>>>>> 5f2a199f6b66f6b8b7fe4048eee24e7e6ea7923f
 }
