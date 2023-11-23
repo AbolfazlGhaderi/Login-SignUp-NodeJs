63d02582fada288a8a925e0fdf5d50d659abbc64
mr.Brian <brianprogrammer0@gmail.com>
2023-11-23 20:51:09 +0330

    Due to some problems, the program returned to the pre-validation stage

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
-const bodyParser = require('body-parser');
-const {validateSingUp} = require('../validations/userValidation');
+
+
+
 // ---------------------- GET ----------------------
 exports.getSingin = (req,res)=>{
     
@@ -9,25 +10,19 @@ exports.getSingin = (req,res)=>{
 exports.getSingup = (req,res)=>{
     // let successAddProduct=req.flash("successAddProduct","test");
     res.render('../views/Singup.ejs',{
-        validateError:''
+        errors:null
     })
 }
 
 // ---------------------- Post ----------------------
 
-exports.postSingUp = async (req,res)=>{
-    // const body = req.body;
-    // console.log(body);
 
-    // const {error,value}=validateSingUp(body,{ebortErly:false});
-    // if(error){
-    //     console.log(error);
-    //     return res.send(error.detals);
-    // }
-    const body = await req.body 
-   console.log(req.body);
-
+exports.postSingUp =  (req,res)=>{
+   
+    console.log(req.body);
    res.json(req.body);
 }
+
+
 
 
--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express=require('express');
 const router=express.Router();
 const authRouter=require('../controllers/auth');
 
+const {check,validationResult} = require('express-validator');
 
 // ---------------------- GET ----------------------
 
