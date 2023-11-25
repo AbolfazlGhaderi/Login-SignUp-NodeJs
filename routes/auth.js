f2c088330b42c04d50d93548382ebb340a62f4b9
mr.Brian <brianprogrammer0@gmail.com>
2023-11-25 09:18:16 +0330

    Update -> The user registration validation section has been successfully completed

--- routes/auth.js
+++ routes/auth.js
@@ -13,10 +13,10 @@ router.get('/singup',authRouter.getSingup)
 // ---------------------- Post ----------------------
 
 router.post('/singup',[
-    check('userName').isLength({min:3}).withMessage(" فیلد خالی است یا تعداد حروف باید از 3 حرف بیشتر باشد "),
+    check('userName').isLength({min:3}).withMessage(" فیلد نام کاربری خالی است / تعداد حروف  باید از 3 حرف بیشتر باشد "),
     check('email').isEmail().withMessage("ایمیل را با دقت وارد کنید"),
-    check('phoneNumber').isLength({min:11,max:11}).withMessage("فیلد خالی است یا تعداد ارقام تلفن همراه باید 11 رقم باشد"),
-    check('password').isLength({min:3}).withMessage("فیلد خالی است یا تعداد حروف پسورد باید از 3 حرف بیشتر باشد "),
+    check('phoneNumber').isLength({min:11,max:11}).isInt().withMessage(" فیلد تلفن همراه خالی است / یا تعداد ارقام تلفن همراه باید 11 رقم باشد / عدد باید وارد کنید"),
+    check('password').isLength({min:3}).withMessage("فیلد خالی است / تعداد حروف پسورد باید از 3 حرف بیشتر باشد "),
 
 ],authRouter.postSingUp)
 
