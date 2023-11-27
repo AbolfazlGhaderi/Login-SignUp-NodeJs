62fcd4f65c7fc7910c06696e902d332f99d46069
mr.Brian <brianprogrammer0@gmail.com>
2023-11-27 10:18:44 +0330

    Add -> SinginOTP Add -> Validation of the mobile phone part is completed

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,72 +1,95 @@
-
-const {validationResult} =require('express-validator');
+const { validationResult } = require('express-validator');
 // ---------------------- GET ----------------------
-exports.getSingin = (req,res)=>{
-    
-    res.render('../views/Singin.ejs',{
-        errors:null,
-        oldItemSIN :{
-            email:null,
-            password:null
+
+exports.getSingin = (req, res) => {
+
+    res.render('../views/Singin.ejs', {
+        errors: null,
+        oldItemSIN: {
+            email: null,
+            password: null
         }
     })
 }
 
-exports.getSingup = (req,res)=>{
+exports.getSingup = (req, res) => {
     // let successAddProduct=req.flash("successAddProduct","test");
-    res.render('../views/Singup.ejs',{
-        errors:null,
-        oldItemSUP:{
-            userName:null,
-            email:null,
-            phoneNumber:null,
-            password:null
+    res.render('../views/Singup.ejs', {
+        errors: null,
+        oldItemSUP: {
+            userName: null,
+            email: null,
+            phoneNumber: null,
+            password: null
         }
     })
 }
 
+exports.getSingInOTP = (req, res) => {
+    res.render('../views/SinginOTP.ejs',{
+        errors:null,
+        oldItemSINOTP:{
+            phoneNumber:null
+        }
+    })
+}
 // ---------------------- Post ----------------------
 
+exports.postSingUp = (req, res) => {
 
-exports.postSingUp =  (req,res)=>{
-   
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        console.log(errors.mapped() );
-        return res.render('../views/Singup.ejs',{
-            errors:errors.mapped() ,
-            oldItemSUP:{
-                userName:req.body.userName,
-                email:req.body.email,
-                phoneNumber:req.body.phoneNumber,
-                password:req.body.password
+    if (!errors.isEmpty()) {
+        console.log(errors.mapped());
+        return res.render('../views/Singup.ejs', {
+            errors: errors.mapped(),
+            oldItemSUP: {
+                userName: req.body.userName,
+                email: req.body.email,
+                phoneNumber: req.body.phoneNumber,
+                password: req.body.password
             }
         })
         //-------------------- Test With Postman -----------------
         // res.json(errors.mapped());
     }
     console.log(req.body);
-//    res.json(req.body);
+    //    res.json(req.body);
 }
 
-exports.postSingIn =  (req,res)=>{
-   
+exports.postSingIn = (req, res) => {
+
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        console.log(errors.mapped() );
-        return res.render('../views/Singin.ejs',{
-            errors:errors.mapped() ,
-            oldItemSIN:{
-                email:req.body.email,
-                password:req.body.password
+    if (!errors.isEmpty()) {
+        console.log(errors.mapped());
+        return res.render('../views/Singin.ejs', {
+            errors: errors.mapped(),
+            oldItemSIN: {
+                email: req.body.email,
+                password: req.body.password
             }
         })
 
-      //-------------------- Test With Postman -----------------
-           // res.json(errors.mapped());
+        //-------------------- Test With Postman -----------------
+        // res.json(errors.mapped());
     }
     console.log(req.body);
-//    res.json(req.body);
+    //    res.json(req.body);
+}
+
+exports.postSingInOTP = (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.log(errors.mapped());
+        return res.render('../views/SinginOTP.ejs', {
+            errors: errors.mapped(),
+            oldItemSINOTP: {
+             phoneNumber: req.body.phoneNumber
+            }
+        })
+
+        //-------------------- Test With Postman -----------------
+        // res.json(errors.mapped());
+    }
 }
 
 
--- routes/auth.js
+++ routes/auth.js
@@ -10,12 +10,14 @@ router.get('/singin', authRouter.getSingin)
 
 router.get('/singup', authRouter.getSingup)
 
+router.get('/singinOTP',authRouter.getSingInOTP)
+
 // ---------------------- Post ----------------------
 
 router.post('/singup', [
     body('userName').isLength({ min: 3 }).withMessage(" فیلد نام کاربری خالی است / تعداد حروف  باید از 3 حرف بیشتر باشد "),
     body('email').isEmail().withMessage("ایمیل را با دقت وارد کنید"),
-    body('phoneNumber',' فیلد تلفن همراه خالی است / یا تعداد ارقام تلفن همراه باید 11 رقم باشد و فقط عدد وارد بشود').isLength({ min: 11, max: 11 }).isInt(),
+    body('phoneNumber',' فیلد تلفن همراه خالی است /  تعداد ارقام تلفن همراه باید 11 رقم باشد و فقط عدد وارد بشود').isLength({ min: 11, max: 11 }).isInt(),
     body('password'," فیلد پسور خالی است / پسورد باید ترکیبی از حروف کوچک و بزرگ ، سیمبل ها و اعداد باشد / بیشتر از 8 حرف باشد").isLength({ min: 3 }).isStrongPassword(
         { minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1 }
     ),
@@ -29,5 +31,10 @@ router.post('/singin', [
     ),
 
 ], authRouter.postSingIn)
+
+router.post('/singinOTP', [
+    body('phoneNumber',' فیلد تلفن همراه خالی است /  تعداد ارقام تلفن همراه باید 11 رقم باشد و فقط عدد وارد بشود').isLength({ min: 11, max: 11 }).isInt()
+
+], authRouter.postSingInOTP)
 
 module.exports = router;
